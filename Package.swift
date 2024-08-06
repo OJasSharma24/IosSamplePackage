b526cddc39192ac5c98e70eb48adac433e20ec46
Ojas Sharma <ojas.sharma@bugsmirror.com>
2024-08-06 18:13:04 +0530

    initial commit

--- Package.swift
+++ Package.swift
@@ -5,19 +5,17 @@ import PackageDescription
 
 let package = Package(
     name: "IosSamplePackage",
+    platforms: [
+         .iOS(.v13)
+     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "IosSamplePackage",
-            targets: ["IosSamplePackage"]),
+            targets: ["IosSampleFramework"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "IosSamplePackage"),
-        .testTarget(
-            name: "IosSamplePackageTests",
-            dependencies: ["IosSamplePackage"]),
+        .binaryTarget(
+            name: "IosSampleFramework",
+            path: "./Sources/IosSampePackage/IosSampleFramework.xcframework/ios-arm64/IosSampleFramework.framework"),
     ]
 )
